155dcb3337fd815790b12bdd041a56bb548bb74a
hzzhanyawei <hzzhanyawei@corp.netease.com>
2015-11-18 14:06:39 +0800

    reuse content view change

---
+++ SlideSwitchView/CustomView.h
@@ -0,0 +1,13 @@
+//
+//  CustomView.h
+//  SlideSwitchView
+//
+//  Created by hzzhanyawei on 15/11/18.
+//  Copyright © 2015年 hzzhanyawei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomView : UIView
+
+@end
---
+++ SlideSwitchView/CustomView.m
@@ -0,0 +1,21 @@
+//
+//  CustomView.m
+//  SlideSwitchView
+//
+//  Created by hzzhanyawei on 15/11/18.
+//  Copyright © 2015年 hzzhanyawei. All rights reserved.
+//
+
+#import "CustomView.h"
+
+@implementation CustomView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
