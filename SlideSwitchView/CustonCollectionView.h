a6d9409c72a7c32fc22f7ccbde2e9fa19ef7f030
hzzhanyawei <hzzhanyawei@corp.netease.com>
2016-01-19 15:12:42 +0800

    add collection view

---
+++ SlideSwitchView/CustonCollectionView.h
@@ -0,0 +1,13 @@
+//
+//  CustonCollectionView.h
+//  SlideSwitchView
+//
+//  Created by hzzhanyawei on 16/1/19.
+//  Copyright © 2016年 hzzhanyawei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustonCollectionView : UICollectionView
+
+@end
---
+++ SlideSwitchView/CustonCollectionView.m
@@ -0,0 +1,14 @@
+//
+//  CustonCollectionView.m
+//  SlideSwitchView
+//
+//  Created by hzzhanyawei on 16/1/19.
+//  Copyright © 2016年 hzzhanyawei. All rights reserved.
+//
+
+#import "CustonCollectionView.h"
+
+@implementation CustonCollectionView
+
+
+@end
